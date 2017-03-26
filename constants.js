9798aff80c40d54c61e68c8b35020341f61e134e
Adam Harris <aharris88@users.noreply.github.com>
2017-03-25 21:33:21 -0600

    Add status bar and toolbar

--- constants.js
+++ constants.js
@@ -1,4 +1,9 @@
 export const COLORS = {
   PRIMARY: '#009788',
   PRIMARY_20: 'rgba(0, 150, 136, 0.2)',
+  WHITE: '#FFFFFF',
+};
+
+export const APP = {
+  TITLE: 'Score keeper',
 };
--- index.android.js
+++ index.android.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
 import {
   AppRegistry,
+  StatusBar,
+  ToolbarAndroid,
+  View,
 } from 'react-native';
 import Card from './components/Card';
+import {APP, COLORS} from './constants';
 
 export default class gameCounter extends Component {
   constructor() {
@@ -12,13 +16,11 @@ export default class gameCounter extends Component {
       score1: 0,
       score2: 0,
     };
-    console.log('hi');
 
     this.handleMinus1 = this.handleMinus1.bind(this);
   }
 
   handleScore(options) {
-    console.log(options);
     const {player, add} = options;
     this.setState({
       score1: this.state.score1 + (player === 1 ? add : 0),
@@ -27,21 +29,42 @@ export default class gameCounter extends Component {
   }
 
   handleMinus1() {
-    console.log('handleMinus1');
     this.handleScore({player: 1, add: -1});
   }
 
   render() {
     return (
-      <Card
-        score={this.state.score1}
-        onPressMinus1={this.handleScore.bind(this, {player: 1, add: -1})}
-        onPressMinus5={this.handleScore.bind(this, {player: 1, add: -5})}
-        onPressPlus5={this.handleScore.bind(this, {player: 1, add: 5})}
-        onPressPlus1={this.handleScore.bind(this, {player: 1, add: 1})}
-      />
+      <View style={styles.page}>
+        <StatusBar
+           backgroundColor={COLORS.PRIMARY}
+           barStyle="light-content"
+         />
+        <ToolbarAndroid
+          style={styles.toolbar}
+          title={APP.TITLE}
+          titleColor={COLORS.WHITE}
+        />
+        <Card
+          score={this.state.score1}
+          onPressMinus1={this.handleScore.bind(this, {player: 1, add: -1})}
+          onPressMinus5={this.handleScore.bind(this, {player: 1, add: -5})}
+          onPressPlus5={this.handleScore.bind(this, {player: 1, add: 5})}
+          onPressPlus1={this.handleScore.bind(this, {player: 1, add: 1})}
+        />
+      </View>
     );
   }
 }
+
+const styles = {
+  page: {
+    flex: 1,
+    alignItems: 'stretch',
+  },
+  toolbar: {
+    height: 56,
+    backgroundColor: COLORS.PRIMARY,
+  },
+};
 
 AppRegistry.registerComponent('gameCounter', () => gameCounter);
